21e968e6d916209f0e1e841abde4227a2240eb57
seokeunparks <pppse620@gmail.com>
2023-10-26 08:31:45 +0900

    변수 초기화 예시

---
+++ VariableInitializationExample.java
@@ -0,0 +1,13 @@
+package com.mystudy.run;
+
+public class VariableInitializationExample {
+    public static void main(String[] args) {
+
+        int value=0;
+
+        int result = value + 10;
+
+
+        System.out.println(result);
+    }
+}
