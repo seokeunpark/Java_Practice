6139bf330ec995674d3b249b95d311bb8adc575a
seokeunparks <pppse620@gmail.com>
2023-11-06 10:38:16 +0900

    Exercise3_2

---
+++ Exercise3_2.java
@@ -0,0 +1,9 @@
+public class Exercise3_2 {
+    public static void main(String[] args) {
+        int numOfApples = 123;
+        int sizeOfBucket = 10;
+        int numOfBucket = numOfApples / sizeOfBucket + (numOfApples % sizeOfBucket > 0 ? 1 : 0);
+
+        System.out.println(numOfBucket);
+    }
+}
